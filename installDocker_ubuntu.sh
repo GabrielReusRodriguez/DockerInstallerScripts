41a66f59a03d740c74754e9eeec3b56361c5c9f2
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2022-08-18 23:29:07 +0200

    Agregado  las instrucciones para crear el grupo docker y añadir el usuario que lo ha instalado al grupo

--- installDocker_ubuntu.sh
+++ installDocker_ubuntu.sh
@@ -10,7 +10,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 #2. Actualizo los repositorios APT-get
 sudo apt-get update
 
-#3. Permioto a apt-get utilizar https.
+#3. Permito a apt-get utilizar https.
 sudo apt-get install \
     ca-certificates \
     curl \
@@ -18,7 +18,7 @@ sudo apt-get install \
     lsb-release
 
 
-#4. Añado lña key GPG oficial a la lista de aceptadas
+#4. Añado la key GPG oficial a la lista de aceptadas
 #mkdir -p hace que no salte error si  ya existe y ademas fuerza a crear la estructura de directorios
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -36,6 +36,12 @@ echo \
 #Actualizamos el apt-get
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#Añadimos el usuario al grupo docker  para que no de el error de permisos del socket.
+#Creamos el grupo docker
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+su -s ${USER}
 
 #Verificamos la instalacion con una imagen helloworld
  sudo docker run hello-world 
